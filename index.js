897367a54afb37c7a7c76eb473d553763d79cc96
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2026-04-29 21:32:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express, { json } from 'express';
-import { connect, Schema, model } from 'mongoose';
-import cors from 'cors';
-import 'dotenv/config';
+import express, { json } from "express";
+import { connect, Schema, model } from "mongoose";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 const DB_HOST = process.env.DB_HOST;
@@ -11,101 +11,97 @@ app.use(cors());
 
 connect(DB_HOST)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
   });
 
 const todoSchema = new Schema(
-    {
-  title: String,  
-  completed: Boolean  
-    },
-    { versionKey: false }
+  {
+    title: String,
+    completed: Boolean,
+    order: Number,
+  },
+  { versionKey: false },
 );
 
-const Todo = model('Todo', todoSchema);
+const Todo = model("Todo", todoSchema);
 
 app.use(json());
 
-app.get('/todos', async (_req, res) => {
-    try {
-        const todos = await Todo.find(); 
-        res.json(todos); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+app.get("/todos", async (_req, res) => {
+  try {
+    const todos = await Todo.find().sort({ order: 1 });
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.post('/todos', async (req, res) => {
-    const { title, completed } = req.body;
-
-    try {
-        const newTodo = new Todo({ title, completed });
-        const savedTodo = await newTodo.save(); 
-        res.status(201).json(savedTodo); 
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-
-app.delete('/todos/:_id', async (req, res) => {
-    const { _id } = req.params;
-
-    try {
-        const deletedTodo = await Todo.findByIdAndDelete(_id); 
-        if (!deletedTodo) {
-            return res.status(404).json({ message: 'Todo not found' });
-        }
-        res.json(deletedTodo); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-app.put('/todos/:_id', async (req, res) => {
-    const { _id } = req.params;
-    const { title, completed } = req.body;
-
-    try {
-        const updatedTodo = await Todo.findByIdAndUpdate(
-            _id,
-            { title, completed },
-            { new: true }
-        );
-        if (!updatedTodo) {
-            return res.status(404).json({ message: 'Todo not found' });
-        }
-        res.json(updatedTodo); 
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-app.post('/save-todos', async (req, res) => {
-  const todosArray = req.body; 
+app.post("/todos", async (req, res) => {
+  const { title, completed } = req.body;
 
   try {
-    
-    const todosWithOrder = todosArray.map((todo, index) => ({
-      ...todo,
-      order: index, 
-    }));
-
-    await Todo.deleteMany({});
-
-    const savedTodos = await Todo.insertMany(todosWithOrder);
-
-    res.status(201).json(savedTodos); 
+    const count = await Todo.countDocuments();
+    const newTodo = new Todo({ title, completed, order: count });
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 
+app.delete("/todos/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(_id);
+    if (!deletedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(deletedTodo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/todos/:_id", async (req, res) => {
+  const { _id } = req.params;
+  const { title, completed } = req.body;
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      _id,
+      { title, completed },
+      { new: true },
+    );
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/save-todos", async (req, res) => {
+  const todosArray = req.body;
+
+  try {
+    const todosWithOrder = todosArray.map((todo, index) => ({
+      ...todo,
+      order: index,
+    }));
+
+    await Todo.deleteMany({});
+    const savedTodos = await Todo.insertMany(todosWithOrder);
+
+    res.status(201).json(savedTodos);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
